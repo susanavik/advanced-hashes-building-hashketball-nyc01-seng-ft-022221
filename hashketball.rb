58b583909fc3dee51ddaa2d13c261b1ad369b4e4
Susana vik <susana.m.vik@gmail.com>
2021-02-14 18:09:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,13 +1,35 @@
 def game_hash
   game_hash = {
     :home=> {
-      :team_name,
-      :colors,
-      :players
+      :team_name=> "",
+      :colors = [],
+      :players = [
+        {:player_name=>},
+        {:number=>},
+        {:shoe=>},
+        {:points=>},
+        {:rebounds},
+        {:assists},
+        {:steals},
+        {:blocks=>},
+        {:slam_dunks=>}
+      ]
     },
     :away=> {
-      :team_name,
-      :colors,
-      :players
+      :team_name=>" ",
+      :colors = [
+        
+      ],
+      :players = [
+        {:player_name=>},
+        {:number=>},
+        {:shoe=>},
+        {:points=>},
+        {:rebounds},
+        {:assists},
+        {:steals},
+        {:blocks=>},
+        {:slam_dunks=>}
+      ]
     }
   }
